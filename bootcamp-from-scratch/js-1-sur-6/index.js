0de3032c805d9d27c63db768152d6c19829bb233
sow <yeroscorpion149@gmail.com>
2024-08-09 16:33:52 +0100

    learning variables topics

---
+++ bootcamp-from-scratch/js-1-sur-6/index.js
@@ -0,0 +1,8 @@
+//* ------------- les variables -----------------------
+// var
+// let
+// const
+// echapper caractere
+// concatenation (les plus, les backtic (alg gr+7))
+
+//* ------------- les types de donnees -----------------------
