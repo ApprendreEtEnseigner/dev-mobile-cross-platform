b0a992d4fd1fcb1586e049059f38c73466c32bd4
sow <yeroscorpion149@gmail.com>
2024-08-13 18:16:10 +0100

    completed crs js 1/6

--- bootcamp-from-scratch/js-1-sur-6/index.js
+++ bootcamp-from-scratch/js-1-sur-6/index.js
@@ -6,3 +6,62 @@
 // concatenation (les plus, les backtic (alg gr+7))
 
 //* ------------- les types de donnees -----------------------
+// String
+// Number
+// Boolean
+// Array
+// Object
+
+//* ------------- les operateurs arithemetiques -----------------------
+// console.log(4 + 5);
+// console.log(4 - 5);
+// console.log(4 / 5);
+// console.log(4 * 5);
+// console.log(4 ** 5);
+
+//* ------------- les operateurs -----------------------
+// let total = 0;
+// total++;
+// total +=5;
+// total -=5;
+// total *=5;
+
+//* ------------- les structures de control -----------------------
+let x = 2;
+let y = 3;
+
+// if (x > y){
+//     alert("Yes x > y")
+// } else if ( x < y) {
+//     alert("Yes x < y")
+// } else {
+//     alert("ils sont egaux")
+// }
+
+// if (x) {
+//     console.log("x exist");
+    
+// }
+
+// === egalite en type et valeur 
+// == egalite en valeur uniquement 
+// || ou 
+// && et 
+
+
+//* ------------- les fonctions -----------------------
+//A Fonction classique 
+// 1. definition
+function faireQulequeChose(){
+    console.log("fais !");
+};
+// 2. appel
+faireQulequeChose();
+
+//B Fonction flechee 
+const addition = (a, b) => {
+    console.log(a+b);
+    
+};
+addition(2, 5);
+//* ------------- la portee variables -----------------------
